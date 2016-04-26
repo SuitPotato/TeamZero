1d663e8c4980bbc03476ffc64d8112008ea84a45
スーツ芋 <Erekyth@yahoo.com>
2016-04-26 08:06:38 -0700

    Datastore Update for Presentation
    Populated page with a couple of dummy points of data for demonstration

--- TeamZero/models/task.py
+++ TeamZero/models/task.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import ndb
 from google.appengine.api import users
 
+
 class Task(ndb.Model):
 	name = ndb.StringProperty(required=True, indexed = True)
     date = ndb.DateProperty(auto_now_add=True, indexed=True)
