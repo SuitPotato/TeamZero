2f24546868dbd4dbbd4783461092dc2213939a7c
monsoonfan <mitchell_ralston@yahoo.com>
2016-04-27 12:25:14 -0700

    Finish change to "Browse All" (store) display
    Add knowledge as well as task, this finishes the changes to make the
    store look like the old details page (table of results)

--- TeamZero/handlers/store_handler.py
+++ TeamZero/handlers/store_handler.py
@@ -7,7 +7,7 @@ class StoreHandler(BaseHandler):
 	def get(self):
 		user = users.get_current_user()
 		items = Item.query().filter(Item.type == 'Task').order(-Item.date).fetch()
-#		items2 = Item.query().filter(Item.type == 'Knowledge').order(-Item.date).fetch()
+		items += Item.query().filter(Item.type == 'Knowledge').order(-Item.date).fetch()
 		
 		template_values = {
 			'items':items,
