a39b70a784b91f29e5c39709a8ac610674d2946d
monsoonfan <mitchell_ralston@yahoo.com>
2016-04-27 22:53:26 -0700

    Filter for Knowledge not tasks
    Code was passing in tasks to be displayed on knowledge details page

--- TeamZero/handlers/store_details_knowledge.py
+++ TeamZero/handlers/store_details_knowledge.py
@@ -6,7 +6,7 @@ from models import Item
 class KnowledgeDetails(BaseHandler):
 	def get(self):
 		user = users.get_current_user()
-		items = Item.query().filter(Item.type == 'Task').order(-Item.date).fetch()
+		items = Item.query().filter(Item.type == 'Knowledge').order(-Item.date).fetch()
 
 		template_values = {
 			'items':items,
