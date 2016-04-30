f6beea4a56cc0f4934ed399863dc818244ade417
monsoonfan <mitchell_ralston@yahoo.com>
2016-04-29 17:44:01 -0700

    pass clicked on task to details page
    make task details specific to task by passing the clicked info of
    clicked on task from task store to task details handler page and
    finding the matching item from the datastore, then passing that item to
    the task details page

--- TeamZero/handlers/store_details_task.py
+++ TeamZero/handlers/store_details_task.py
@@ -1,4 +1,6 @@
 import webapp2
+import re
+
 from google.appengine.api import users
 from handlers import BaseHandler
 from models import Item
@@ -7,9 +9,35 @@ class TaskDetails(BaseHandler):
 	def get(self):
 		user = users.get_current_user()
 		items = Item.query().filter(Item.type == 'Task').order(-Item.date).fetch()
+		citem = self.request.get('citem')
+		match = re.search(r'(\d+)',citem).group(0)
+		
+		for item in items:
+			name = item.name
+			type = item.type
+			owner = item.owner
+			description = item.description
+			key = item.key
+			id = re.search(r'(\d+)',str(key)).group(0)
+			if id == match:
+				passitem = item
+
+# stuff that didnt work:
+		#citem = Item.query('agxkZXZ-dGVhbXplcm9yEQsSBEl0ZW0YgICAgIDQhwgM')
+		#itemkey = item.key()
+		#Key('Item', 4572319104106496)	
+		#item = Item.query().filter('__key__' == match)
+		#item = Item.get_by_key_name(match)
+		#item = Item.get_by_id(match)
 
 		template_values = {
 			'items':items,
-            'user':user
+            'user':user,
+            'name':name,
+            'type':type,
+            'owner':owner,
+            'description':description,
+            'passitem':passitem
 		}
+		
 		self.render("details_task.html", (template_values))
